703676d15bdcaadc376eb429cd82b67053574e7a
Ambar <ambarmogg@me.com>
2020-11-04 17:27:10 -0800

    second product

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 product = Product.new(name: "Apple Macbook Pro", price: 1299, image_url: "https://images-na.ssl-images-amazon.com/images/I/71BMVOs2xML._AC_SL1500_.jpg", description: "2020 Space Gray 13.3 inch Apple with Retina Display")
+
+product = Product.new(name: "HP ProBook 450 G7", price: 1119, image_url: "https://static.bhphoto.com/images/images500x500/hp_8wc04ut_aba_hp_powerbook_450_g7_15_6_1579005466_1538620.jpg", description: "HP 15.6 ProBook 450 G7 Laptop")
 product.save
 
-p product
