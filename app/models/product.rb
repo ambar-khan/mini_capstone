5baf4873d0a3a4ff35033cc0ee60096098f91e97
Ambar <ambarmogg@me.com>
2020-11-10 19:47:43 -0800

    added model method tax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  def product_tax
+    tax = price * 0.09
+    total = price + tax
+    "Price for computer: $#{price}, tax at 9%: $#{tax}. Total cost for computer including tax is $#{total}"
+  end
 
- 
 end
