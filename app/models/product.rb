8d537fe11b1c3747728d0fdad9bb99a63e2234e3
Ambar <ambarmogg@me.com>
2020-11-11 16:32:20 -0800

    add total model method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ApplicationRecord
   def product_tax
     tax = price * 0.09
-    total = price + tax
-    "Price for computer: $#{price}, tax at 9%: $#{tax}. Total cost for computer including tax is $#{total}"
   end
 
   def is_discounted
@@ -11,6 +9,12 @@ class Product < ApplicationRecord
    else
      "False"
    end
+  end
+
+  def total
+    tax = price * 0.09
+    total = price + tax
+    "Price for computer: $#{price}, tax at 9%: $#{tax}. Total cost for computer including tax is $#{total}"
   end
 
 end
