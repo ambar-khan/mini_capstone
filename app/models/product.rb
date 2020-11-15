0e62e5ac7b72df4b4ad7964506f1323b112f22a1
Ambar <ambarmogg@me.com>
2020-11-15 08:15:25 -0800

    add validations and error messages 422

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, :price, :description, presence: true
+  validates :price, numericality: { :greater_than_or_equal_to => 0 }
+  validates :description, length: { in: 10..500}
+
+  
   def product_tax
     tax = price * 0.09
   end
