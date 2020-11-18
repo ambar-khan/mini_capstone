ed202ac3a02ac53d3386ff746b94a1e8b560a275
Ambar <ambarmogg@me.com>
2020-11-17 17:54:50 -0800

    added Images model to have each product display multiple images

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   validates :price, numericality: { :greater_than_or_equal_to => 0 }
   validates :description, length: { in: 10..500}
   belongs_to :supplier
+  has_many :images
 
   # def supplier
   #   Supplier.find_by(id: supplier_id) 
