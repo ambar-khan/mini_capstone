315448dad82c4410f31c342b0dd43f7b05f8a1eb
Ambar <ambarmogg@me.com>
2020-11-23 17:31:06 -0800

    add has_many through associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
   
   def tax
     price * 0.09
