5c85b0226a3c1b5aa2afcfa4399d3c2414ff4cd4
Ambar <ambarmogg@me.com>
2020-11-24 17:43:01 -0800

    add cartedproduct model and cotnroller remove product_id and quantity from order model add belongs to cartedproducts make create route for carted product

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
 
     post "/orders" => "orders#create"
+
+    post "/carted_products" => "carted_products#create"
   end
 end
