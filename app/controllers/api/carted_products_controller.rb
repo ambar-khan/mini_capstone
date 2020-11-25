5c69024ea81cb41c493d5117504899dd790e65c6
Ambar <ambarmogg@me.com>
2020-11-24 19:51:01 -0800

    add carted products index where only user's items are in shopping cart

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -2,17 +2,18 @@ class Api::CartedProductsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    # product = Product.find_by(id: params[:product_id])
-    
+  
     @carted_product = CartedProduct.create(
+      user_id: current_user.id,
       product_id: params[:product_id], 
       quantity: params[:quantity],
+      status: "carted"
     ) 
-    render "index.json.jb"
+    render "show.json.jb"
   end
 
   def index
-    render json: {message: "This works"}
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render 'index.json.jb'
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :order
+  has_many :carted_products
 
 end
